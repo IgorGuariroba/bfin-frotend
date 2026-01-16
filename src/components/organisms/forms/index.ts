1dbb7e8216ecf35f67ce29dbfff79f824664e086
igor.guariroba <igor.guariroba@jsl.com.br>
2026-01-16 16:07:18 -0300

    style: standardize BfinParceiroForm info box with brand colors and Zap icon

--- src/components/organisms/forms/index.ts
+++ src/components/organisms/forms/index.ts
@@ -3,3 +3,4 @@ export { CreateAccountForm } from './CreateAccountForm';
 export { IncomeForm } from './IncomeForm';
 export { FixedExpenseForm } from './FixedExpenseForm';
 export { VariableExpenseForm } from './VariableExpenseForm';
+export { BfinParceiroForm } from './BfinParceiroForm';
